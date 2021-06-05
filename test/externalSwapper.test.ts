02da4e076456741704d6c29f05b983e2f3bae3f4
SevenSwen <tihonenko.v.v@gmail.com>
2021-06-05 22:22:42 +0700

    - test https://etherscan.io/tx/0x3f909b671aeb3a528cb8104d99579765b739c2babcd7e8f5835c9a1855fc9523

--- test/externalSwapper.test.ts
+++ test/externalSwapper.test.ts
@@ -84,10 +84,53 @@ describe('test', () => {
 
         expect(await token1.balanceOf(externalSwapper.address)).to.equal(ethers.utils.parseEther('0'))
         const balance = await token0.balanceOf(externalSwapper.address);
-        console.log('(swap 1 WBTC) DAI balance:', ethers.utils.formatEther(balance));
+        console.log('profit:', ethers.utils.formatEther(balance));
     })
 
     it('test', async () => {
+        await token0.approve(uniswapRouter.address, ethers.utils.parseEther('479096246133.332078885965615880'))
+        await token1.approve(uniswapRouter.address, ethers.utils.parseEther('2035.106916166573058792'))
+        await uniswapRouter.addLiquidity(
+            token0.address,
+            token1.address,
+            ethers.utils.parseEther('479096246133.332078885965615880'),
+            ethers.utils.parseEther('2035.106916166573058792'),
+            0,
+            0,
+            alice.address,
+            1923572523, // TODO: change to now
+        )
 
+        await token0.approve(sushiswapRouter.address, ethers.utils.parseEther('50340792.388814552382923537'))
+        await token1.approve(sushiswapRouter.address, ethers.utils.parseEther('0.091072766630374925'))
+        await sushiswapRouter.addLiquidity(
+            token0.address,
+            token1.address,
+            ethers.utils.parseEther('50340792.388814552382923537'),
+            ethers.utils.parseEther('0.091072766630374925'),
+            0,
+            0,
+            alice.address,
+            1923572523, // TODO: change to now
+        )
+        expect(await token0.balanceOf(externalSwapper.address)).to.equal(ethers.utils.parseEther('0'))
+        expect(await token1.balanceOf(externalSwapper.address)).to.equal(ethers.utils.parseEther('0'))
+
+        const amount = ethers.utils.parseEther('17402539.933738453216109790');
+        const amountRequired = (await sushiswapRouter.getAmountsIn(amount, [token1.address, token0.address]))[0];
+
+        await externalSwapper.connect(alice).flashLoan({
+            tokenIn: token0.address,
+            tokenOut: token1.address,
+            pair: await sushiswapFactory.getPair(token0.address, token1.address),
+            routerOut: uniswapRouter.address,
+            amountRequired: amountRequired,
+            amount: amount,
+            deadline: 1923572523, // TODO: change to now
+        })
+
+        expect(await token0.balanceOf(externalSwapper.address)).to.equal(ethers.utils.parseEther('0'))
+        const balance = await token1.balanceOf(externalSwapper.address);
+        console.log('profit:', ethers.utils.formatEther(balance));
     })
 })
